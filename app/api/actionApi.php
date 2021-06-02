63b1a817f96625aa5ccf994b1dea6013c1207ed4
imrahulmane <manerahul8298@gmail.com>
2021-06-02 19:47:11 +0530

    completed all API's

--- app/api/actionApi.php
+++ app/api/actionApi.php
@@ -13,30 +13,45 @@ $app = new App($configuration);
 $app->post('/action', function ($request, $response){
     $data = $request->getParsedBody();
     $actionController = new ActionController();
+    $result = $actionController->addAction($data);
+    return $response->withJson($result);
 });
 $app->put('/action/{action_id}', function ($request, $response){
     $actionId = $request->getAttribute('action_id');
     $data = $request->getParsedBody();
     $actionController = new ActionController();
+    $result = $actionController->updateAction($actionId, $data);
+    return $response->withJson($result);
 });
 $app->get('/action/{action_id}', function ($request, $response){
     $actionId = $request->getAttribute('action_id');
     $actionController = new ActionController();
+    $result = $actionController->getAction($actionId);
+    return $response->withJson($result);
 });
 $app->get('/action', function ($request, $response){
     $actionController = new ActionController();
+    $result = $actionController->getAllActions();
+    return $response->withJson($result);
 });
 $app->delete('/action/{action_id}', function ($request, $response){
     $actionId = $request->getAttribute('action_id');
     $actionController = new ActionController();
+    $result = $actionController->deleteAction($actionId);
+    return $response->withJson($result);
 });
 $app->patch('/action/{action_id}', function ($request, $response){
     $actionId = $request->getAttribute('action_id');
     $actionController = new ActionController();
+    $result = $actionController->closeAction($actionId);
+    return $response->withJson($result);
 });
 $app->get('/actions/search', function ($request, $response){
     $data = $request->getParam('data');
+    $data = json_decode($data, 1);
     $actionController = new ActionController();
+    $result = $actionController->searchAction($data);
+    return $response->withJson($result);
 });
 
 //$app->post('/', function ($request, $response){});
--- app/controllers/ActionController.php
+++ app/controllers/ActionController.php
@@ -4,13 +4,181 @@
 namespace App\controllers;
 
 
+use App\providers\ActionDataProvider;
+use App\providers\CompanyDataProvider;
+use App\providers\CounterDataProvider;
+use App\providers\CustomerDataProvider;
+use App\util\BaseDataProvider;
+use MongoDB\BSON\ObjectId;
+
 class ActionController
 {
-    public function addAction($data){}
-    public function updateAction($actionId, $data){}
-    public function getAction($actionId){}
-    public function getAllActions(){}
-    public function deleteAction($actionId){}
-    public function closeAction($actionId){}
-    public function searchAction($data){}
+    public function addAction(&$data){
+        $actionDataProvider = new ActionDataProvider();
+        $counter = $this->getCounter();
+        $latestCounter = $counter + 1;
+
+        $data['action_id'] = $latestCounter;
+        $result = $actionDataProvider->insertOne($data);
+
+        if(!$result) {
+            return [
+                'status' => 'failed',
+                'message' => 'There is problem in inserting data'
+            ];
+        }
+
+        $this->updateCounter($latestCounter);
+
+        return[
+            'status' => 'success',
+            'message' => 'Action created successfully'
+        ];
+    }
+
+    public function updateAction($actionId, $data){
+        $actionDataProvider = new ActionDataProvider();
+        $searchArray = ['_id' => new ObjectId($actionId)];
+        $updateArray = ['$set' => $data];
+        $result = $actionDataProvider->updateOne($searchArray, $updateArray);
+
+        if($result == 0) {
+            return [
+                'status' => 'failed',
+                'message' => 'Please provide valid ID or Data'
+            ];
+        }
+
+        return [
+            'status' => 'success',
+            'message' => 'Action updated successfully'
+        ];
+
+    }
+
+    public function getAction($actionId){
+        $actionDataProvider = new ActionDataProvider();
+        $searchArray = ['_id' => new ObjectId($actionId)];
+        $action = $actionDataProvider->findOne($searchArray);
+
+        $customerAndCompanyName = $this->getCustomerAndCompany($action['customer_id']);
+        $customerName = $customerAndCompanyName['customerFullName'];
+        $companyName = $customerAndCompanyName['companyName'];
+
+        unset($action['customer_id']);
+        $action['customer_name'] = $customerName;
+        $action['company_name'] = $companyName;
+
+        return $action;
+    }
+
+    public function getAllActions(){
+        $actionDataProvider = new ActionDataProvider();
+        $actions = $actionDataProvider->find();
+
+        if(empty($actions)) {
+            return [
+              'status' => 'failed',
+              'message' => 'There are no Actions available'
+            ];
+        }
+
+        foreach ($actions as $key => $action) {
+            $customerAndCompanyName = $this->getCustomerAndCompany($action['customer_id']);
+            $customerName = $customerAndCompanyName['customerFullName'];
+            $companyName = $customerAndCompanyName['companyName'];
+
+            unset($actions[$key]['customer_id']);
+            $actions[$key]['customer_name'] = $customerName;
+            $actions[$key]['company_name'] = $companyName;
+        }
+
+        return $actions;
+    }
+    public function deleteAction($actionId){
+        $actionDataProvider = new ActionDataProvider();
+        $searchArray = ['_id' => new ObjectId($actionId)];
+        $isDeleted = $actionDataProvider->deleteOne($searchArray);
+
+        if($isDeleted == 0) {
+            return[
+                'status' => 'failed',
+                'message' => 'Please provide valid ID'
+            ];
+        }
+
+        return [
+            'status' => 'success',
+            'message' => 'Action is deleted successfully'
+        ];
+
+    }
+    public function closeAction($actionId){
+        $actionDataProvider = new ActionDataProvider();
+        $searchArray = ['_id' => new ObjectId($actionId)];
+        $updateArray = ['$set' => ['status' => 'close']];
+        $isUpdated = $actionDataProvider->updateOne($searchArray, $updateArray);
+
+        if($isUpdated == 0) {
+            return[
+                'status' => 'failed',
+                'message' => 'Please provide valid ID'
+            ];
+        }
+
+        return [
+            'status' => 'success',
+            'message' => 'Action is closed successfully'
+        ];
+    }
+    public function searchAction($data){
+        $actionDataProvider = new ActionDataProvider();
+        $searchArray = $data;
+        $result = $actionDataProvider->find($searchArray);
+
+        if($result == false) {
+            return[
+                'status' => 'failed',
+                'message' => 'Please provide valid data to search'
+            ];
+        }
+
+        return $result;
+
+    }
+
+    private function getCounter(){
+        $counterDataProvider = new CounterDataProvider();
+        $searchArray = ['type' => 2];
+        $counterObject = $counterDataProvider->findOne($searchArray);
+        return $counterObject['counter'];
+    }
+
+    private function updateCounter($latestCounter){
+        $counterDataProvider = new CounterDataProvider();
+        $searchArray = ['type' => 2];
+        $updateArray = ['$set' => ['counter' => $latestCounter]];
+        $counterDataProvider->updateOne($searchArray, $updateArray);
+    }
+
+    private function getCustomerAndCompany($customerId){
+        $customerDataProvider = new CustomerDataProvider();
+        $searchArray = ['_id' => new ObjectId($customerId)];
+        $projection = ['_id' => 0, 'company_id' => 1,
+            'first_name' => 1, 'middle_name' => 1, 'last_name' => 1];
+        $customer = $customerDataProvider->findOne($searchArray, $projection);
+        $customerFullName = $customer['first_name']  . ' ' . $customer['middle_name'] . ' '. $customer['last_name'];
+        $companyName = $this->getCompanyName($customer['company_id']);
+
+        return ['customerFullName' => $customerFullName, 'companyName' => $companyName];
+    }
+
+    private function getCompanyName($companyId){
+        $companyDataProvider = new CompanyDataProvider();
+        $searchArray = ['_id' => new ObjectId($companyId)];
+        $projection = ['_id' => 0, 'name' => 1];
+        $company = $companyDataProvider->findOne($searchArray, $projection);
+        return $company['name'];
+    }
+
 }
